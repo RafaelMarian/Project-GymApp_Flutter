e95b860eedb8de98090d91f4b16aa0fbacc04314
Rafael Vanca <vanca.rafael.marian@gmail.com>
2024-09-13 16:44:17 +0300

    water working

--- lib/water_tracking_page.dart
+++ lib/water_tracking_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'custom_circle_for_steps.dart'; // Ensure this file is in your project and adjust the import if necessary
 
 class WaterTrackingPage extends StatefulWidget {
   @override
@@ -6,103 +8,216 @@ class WaterTrackingPage extends StatefulWidget {
 }
 
 class _WaterTrackingPageState extends State<WaterTrackingPage> {
-  int _selectedGoal = 2000; // Default goal in ml
-  int _currentIntake = 0; // Current water intake
-  double _progress = 0.0;
+  final TextEditingController _waterController = TextEditingController();
+  int _currentIntake = 0;
+  int _goal = 2000; // Default goal in ml
+  double _progress = 0;
+  List<Map<String, dynamic>> _waterHistory = []; // To store history data
 
-  void _updateWaterIntake(int amount) {
-    setState(() {
-      _currentIntake += amount;
-      if (_currentIntake > _selectedGoal) {
-        _currentIntake = _selectedGoal;
+  @override
+  void initState() {
+    super.initState();
+    _fetchData();
+  }
+
+  Future<void> _fetchData() async {
+    final now = DateTime.now();
+    final startDate = now.subtract(Duration(days: 7));
+
+    try {
+      final snapshot = await FirebaseFirestore.instance
+          .collection('water_data')
+          .where('timestamp', isGreaterThanOrEqualTo: Timestamp.fromDate(startDate))
+          .orderBy('timestamp', descending: true)
+          .get();
+
+      List<Map<String, dynamic>> history = [];
+      int latestIntake = 0; // Variable to store the most recent intake data
+
+      for (var doc in snapshot.docs) {
+        final data = doc.data();
+        final intake = data['intake'] as int?;
+        final timestamp = (data['timestamp'] as Timestamp).toDate();
+        if (intake != null) {
+          history.add({
+            'date': '${timestamp.year}-${timestamp.month}-${timestamp.day}',
+            'intake': intake,
+          });
+          // Update latestIntake with the most recent entry
+          latestIntake = intake;
+        }
       }
-      _progress = _currentIntake / _selectedGoal;
+      setState(() {
+        _waterHistory = history;
+        _currentIntake = latestIntake; // Set the latest intake as today's intake
+        _progress = _currentIntake / _goal;
+      });
+    } catch (e) {
+      print('Error fetching water data: $e');
+    }
+  }
+
+  void _updateWaterIntake() {
+    final intake = int.tryParse(_waterController.text) ?? 0;
+    setState(() {
+      _currentIntake += intake;
+      if (_currentIntake > _goal) {
+        _currentIntake = _goal;
+      }
+      _progress = _currentIntake / _goal;
+    });
+
+    FirebaseFirestore.instance.collection('water_data').add({
+      'intake': _currentIntake,
+      'timestamp': Timestamp.now(),
     });
   }
 
-  void _resetWaterIntake() {
-    setState(() {
-      _currentIntake = 0;
-      _progress = 0.0;
-    });
+  Future<void> _clearData() async {
+    bool confirmDelete = await _showConfirmationDialog();
+    if (confirmDelete) {
+      FirebaseFirestore.instance.collection('water_data').get().then((snapshot) {
+        for (DocumentSnapshot ds in snapshot.docs) {
+          ds.reference.delete();
+        }
+      });
+
+      setState(() {
+        _currentIntake = 0;
+        _progress = 0;
+        _waterHistory.clear(); // Clear the history as well
+      });
+    }
+  }
+
+  Future<bool> _showConfirmationDialog() async {
+    return await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Confirm Deletion"),
+          content: Text("Are you sure you want to clear all data? This action cannot be undone."),
+          actions: [
+            TextButton(
+              child: Text("Cancel"),
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+            ),
+            TextButton(
+              child: Text("Delete"),
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Water Tracking'),
-        backgroundColor: Colors.grey[800],
-      ),
-      backgroundColor: Colors.grey[800],
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Text(
-              'Select Your Water Goal (ml)',
-              style: TextStyle(fontSize: 18, color: Colors.yellow),
-            ),
-            const SizedBox(height: 10),
-            DropdownButton<int>(
-              value: _selectedGoal,
-              items: List.generate(10, (index) {
-                int goalValue = (index + 1) * 500;
-                return DropdownMenuItem(
-                  value: goalValue,
-                  child: Text(
-                    '$goalValue ml',
-                    style: const TextStyle(color: Colors.yellow),
-                  ),
-                );
-              }),
-              onChanged: (int? newValue) {
-                setState(() {
-                  _selectedGoal = newValue!;
-                  _progress = _currentIntake / _selectedGoal;
-                });
-              },
-              dropdownColor: Colors.grey[800],
-              iconEnabledColor: Colors.yellow,
-            ),
-            const SizedBox(height: 30),
-            Text(
-              'Current Intake: $_currentIntake ml',
-              style: const TextStyle(fontSize: 18, color: Colors.yellow),
-            ),
-            const SizedBox(height: 20),
-            LinearProgressIndicator(
-              value: _progress,
-              backgroundColor: Colors.grey,
-              color: Colors.yellow,
-              minHeight: 20,
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () => _updateWaterIntake(250),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.yellow,
-              ),
-              child: const Text('Add 250 ml'),
-            ),
-            ElevatedButton(
-              onPressed: () => _updateWaterIntake(500),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.yellow,
-              ),
-              child: const Text('Add 500 ml'),
-            ),
-            const Spacer(), // Pushes the reset button down
-            ElevatedButton(
-              onPressed: _resetWaterIntake,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.red,
-              ),
-              child: const Text('Reset Intake'),
-            ),
-          ],
+        title: const Text(
+          'Water Tracking',
+          style: TextStyle(color: Colors.white), // Set text color to white
         ),
+        backgroundColor: Colors.grey[800],
+        centerTitle: true, // Center the title
+      ),
+      body: Container(
+        color: Colors.black, // Set background color to black
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CustomCircularProgress(
+                progress: _progress.clamp(0.0, 1.0),
+                size: 150, // Same size as steps counter
+                backgroundColor: Colors.grey[400]!,
+                progressColor: Colors.yellow,
+              ),
+              const SizedBox(height: 20),
+              Text(
+                '$_currentIntake ml',
+                style: const TextStyle(fontSize: 36, color: Colors.white), // Increased font size
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                controller: _waterController,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Enter Water Intake (ml)',
+                ),
+              ),
+              const SizedBox(height: 20),
+              DropdownButton<int>(
+                value: _goal,
+                items: [1000, 2000, 3000, 4000, 5000].map((int value) {
+                  return DropdownMenuItem<int>(
+                    value: value,
+                    child: Text('$value ml'),
+                  );
+                }).toList(),
+                onChanged: (newValue) {
+                  setState(() {
+                    _goal = newValue ?? _goal;
+                  });
+                },
+                hint: const Text('Select your goal'),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                'Goal Progress: ${(_progress * 100).toStringAsFixed(1)}%',
+                style: const TextStyle(fontSize: 18, color: Colors.white),
+              ),
+              const SizedBox(height: 20),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: _waterHistory.length,
+                  itemBuilder: (context, index) {
+                    final entry = _waterHistory[index];
+                    return Container(
+                      margin: EdgeInsets.symmetric(vertical: 5),
+                      child: ListTile(
+                        title: Text(
+                          'Date: ${entry['date']}',
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                        subtitle: Text(
+                          'Intake: ${entry['intake']} ml',
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                        tileColor: Colors.grey[800],
+                        contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: _clearData,
+            child: const Icon(Icons.remove),
+            backgroundColor: Colors.red,
+          ),
+          const SizedBox(height: 10),
+          FloatingActionButton(
+            onPressed: _updateWaterIntake,
+            child: const Icon(Icons.add),
+            backgroundColor: Colors.yellow,
+          ),
+        ],
       ),
     );
   }
